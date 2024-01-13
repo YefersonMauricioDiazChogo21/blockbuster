580d86c554aeede9998474f1cb7f08071fd82691
YefersonMauricioDiazChogo21 <152308421+YefersonMauricioDiazChogo21@users.noreply.github.com>
2024-01-12 20:48:08 -0500

    Actualizar validación.py

--- examen (5)/validación.py
+++ examen (5)/validación.py
@@ -1,1 +1,23 @@
-def validacionInt()
+def validacionint():
+  try:
+    x = int(input())
+    return x
+  except ValueError:
+    print("Ese no es un numero entero.  Intenta de nuevo...")
+    return validacionint()
+
+def validacionfloat():
+  try:
+    x = float(input())
+    return x
+  except ValueError:
+    print("Ese no es un numero decimal.  Intenta de nuevo...")
+    return validacionfloat()
+
+def validacionstr():
+  try:
+    x = str(input())
+    return x
+  except ValueError:
+    print("Ese no es un texto.  Intenta de nuevo...")
+    return validacionstr()
